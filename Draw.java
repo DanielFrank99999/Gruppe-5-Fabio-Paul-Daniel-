e8132f8103c34f454675ecb8656e5a771b59984e
Fabio16739 <rauscher.fabio@gmail.com>
2021-06-14 20:59:27 +0200

    Rand

--- Draw.java
+++ Draw.java
@@ -9,10 +9,10 @@ public class Draw extends JLabel
         g.setColor(Color.white);
         g.drawString(String.valueOf(Clock.temp),20,20);
         g.setColor(Color.gray);
-        g.fillRect(0,0,800,100);
-        g.fillRect(0,0,20,800);
-        g.fillRect(0,0,800,20);
-        g.fillRect(0,600,800,800);
+        g.fillRect(0,0,600,50);
+        g.fillRect(0,0,10,800);
+        g.fillRect(580,0,600,800);
+        g.fillRect(0,550,600,800);
         repaint();
     }
 }
