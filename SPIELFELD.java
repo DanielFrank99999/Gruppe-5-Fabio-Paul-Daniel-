906d66783aceb9fcbf53e39279df7358c71395c1
DanielFrank <danielfrank99999@gmail.com>
2021-06-15 08:19:18 +0200

    1

--- SPIELFELD.java
+++ SPIELFELD.java
@@ -1,18 +1,9 @@
 public class SPIELFELD
 {
-
-         ITEM item = new ITEM();
-         SPIELFELD()
-         {
-             
-         }
-         
-=======
         SPIELFELD()
         {
                 
         }
->>>>>>> e7c6708c61bb32942078211946c5fa5ede44d127
 }
 
  
--- SwingFenster.java
+++ SwingFenster.java
@@ -15,5 +15,19 @@ public class SwingFenster
         draw.setBounds(0,0,600,800);
         fenster.add(draw);
         new Clock();
+        fenster.setSize(600, 400);
+        fenster.setVisible(true);
+        fenster.setSize(600, 800);
+        fenster.setVisible(true);
+        fenster.setTitle("Tetris");
+        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenster.setResizable(false);
+                fenster.setLayout(null);
+                fenster.setLocationRelativeTo(null);
+        Draw draw = new Draw();
+        draw.setBounds(0,0,600,800);
+        fenster.add(draw);
+        new Clock();
+
     }
 }
