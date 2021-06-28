602be9a4c6b260abdd65287133a75334f6ac7bb1
Paul14324 <packpaul@outlook.de>
2021-06-28 22:43:23 +0200

    Item

--- Draw.java
+++ Draw.java
@@ -8,6 +8,8 @@ public class Draw extends JLabel
     private static final int rasterGroesse = 35;
     private static final int hoeheFenster = rasterGroesse * 16;
     private static final int breiteFenster = rasterGroesse * 16;
+    private static Draw o = null;
+    private JFrame fenster = null;
     protected void paintComponent(Graphics g){
         g.setColor(Color.black);
         g.fillRect(0,0,600,800);
@@ -40,5 +42,45 @@ public class Draw extends JLabel
              g. drawLine (breiteFenster / 2, 0, breiteFenster / 2, hoeheFenster - 1);
                 }
         repaint();
+
+
     }
+    public Draw ()
+    {
+        fenster. setResizable (false);
+        fenster. setVisible (true);
+        fenster. setLayout (null);
+        fenster. getContentPane (). setBackground (new Color (240, 240, 240));
+        Insets i = fenster.getInsets();
+        fenster. setSize (breiteFenster, hoeheFenster + i.top);         // Ausgleich für Fenstertitel
+        JComponent hintergrund = new JComponent ()
+        {
+             /**
+             * Zeichnet das Hintergrundmuster.
+             */
+            public void paintComponent (Graphics g)
+            {
+                g. setColor (Color. lightGray);
+                for (int i = 1; i <= hoeheFenster / (rasterGroesse * 2); i++)
+                {
+                    g. drawLine (0, hoeheFenster / 2 + i * rasterGroesse, breiteFenster - 1, hoeheFenster / 2 + i * rasterGroesse);
+                    g. drawLine (0, hoeheFenster / 2 - i * rasterGroesse, breiteFenster - 1, hoeheFenster / 2 - i * rasterGroesse);
+                }
+                for (int i = 1; i <= breiteFenster / (rasterGroesse * 2); i++)
+                {
+                    g. drawLine (breiteFenster / 2 + i * rasterGroesse, 0, breiteFenster / 2 + i * rasterGroesse, hoeheFenster - 1);
+                    g. drawLine (breiteFenster / 2 - i * rasterGroesse, 0, breiteFenster / 2 - i * rasterGroesse, hoeheFenster - 1);
+                }
+                g. setColor (Color. black);
+                g. drawLine (0, hoeheFenster / 2, breiteFenster - 1, hoeheFenster / 2);
+                g. drawLine (breiteFenster / 2, 0, breiteFenster / 2, hoeheFenster - 1);
+            }
+        };
+        hintergrund. setVisible (true);
+        hintergrund. setSize (breiteFenster, hoeheFenster);
+        hintergrund. setLocation (0, 0);
+        fenster. add (hintergrund);
+    }
+
+
 }
--- ITEM.java
+++ ITEM.java
@@ -1,9 +1,14 @@
 
 abstract class ITEM extends Draw
 {
-   
+   int positionX;
+   int positionY;
+   char fall;
    ITEM()
    {
-      
+      positionX = 300;
+      positionY = 0;
+      Runterfallen ();
+      fall = 'O';
    } 
 }
