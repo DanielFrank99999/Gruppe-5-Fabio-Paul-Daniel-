7c34010be5770b19d0f1de0d7674e4967b5b78ac
Paul143214 <packpaul@outlook.de>
2021-06-28 22:36:31 +0200

    Items

--- ITEM.java
+++ ITEM.java
@@ -1,45 +1,9 @@
-import java.util.concurrent.ThreadLocalRandom;
+
 abstract class ITEM extends Draw
 {
-   int positionX;
-   int positionY;
-   char fall;
+   
    ITEM()
    {
-       positionX = 1;
-       positionY = 1;
-       Runterfallen ();
-       fall = 'O';
+       
    } 
-   public static void main(String[] args) 
-   {
-        int min = 1;
-        int max = 10;
-        for(int i = 1; i <=10; i++) 
-        {
-        int getRandomValue = ThreadLocalRandom.current().nextInt(min, max) + min;
-
-        System.out.println(getRandomValue);
-        }
-   }
-   void Runterfallen ()
-   {
-       switch ( fall )
-       {
-           case 'O':
-                break;
-           case 'L':
-                break;
-           case 'S':
-                break;
-           case 'I':
-                break;
-           case 'Z':
-                break;
-           case 'T':
-                break;
-           case 'J':
-                break;
-       }
-   }
 }
--- SPIELFELD.java
+++ SPIELFELD.java
@@ -1,15 +1,21 @@
+
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.JFrame;
 import java.awt.*;
 import javax. swing. *;
-public class SPIELFELD
+
+public class SPIELFELD extends ITEM
 {
-    private static final int rasterGroesse = 35;
-    private static final int hoeheFenster = rasterGroesse * 16;
-    private static final int breiteFenster = rasterGroesse * 16;
-    private static SPIELFELD o = null;
-    private JFrame fenster = null;
-    public static void main(String[] args)
-    {
+   private static final int rasterGroesse = 35;
+   private static final int hoeheFenster = rasterGroesse * 16;
+   private static final int breiteFenster = rasterGroesse * 16;
+   private static SPIELFELD o = null;
+   private JFrame fenster = null;
+   int positionX;
+   int positionY;
+   char fall;
+   public static void main(String[] args)
+   {
         JFrame fenster = new JFrame();
         fenster.setSize(600, 800);
         fenster.setVisible(true);
@@ -22,34 +28,38 @@ public class SPIELFELD
         draw.setBounds(0,0,600,800);
         fenster.add(draw);
         new Clock();
-    }
+        positionX = 1;  
+        positionY = 1;
+        Runterfallen ();
+        fall = 'O';
+   }
     
-    static JFrame FensterGeben ()
-    {
+   static JFrame FensterGeben ()
+   {
         if (o == null)
         {
             o = new SPIELFELD ();
         }
         return o. fenster;
-    }
+   }
 
     /**
      * Gibt die Rastergröße zurück.
      * @return Ausgabefenster
      */
-    static int RasterGroesseGeben ()
-    {
+   static int RasterGroesseGeben ()
+   {
         return rasterGroesse;
-    }
+   }
 
     /**
      * Gibt die Breite des Fensters zurück.
      * @return Fensterbreite
      */
-    static int FensterBreiteGeben ()
-    {
+   static int FensterBreiteGeben ()
+   {
         return breiteFenster;
-    }
+   }
 
     /**
      * Gibt die Höhe des Fensters zurück.
@@ -58,5 +68,45 @@ public class SPIELFELD
     static int FensterHoeheGeben ()
     {
         return hoeheFenster;
-    }
+   }
+   
+   public static void main(String[] args)
+   {
+        java.util.List<String> someList = someMethod();  
+        int min = 1;
+        int max = 10;
+        for(int i = 1; i <=10; i++) 
+        {
+            int getRandomValue = ThreadLocalRandom.current().nextInt(min, max) + min;
+
+            System.out.println(getRandomValue);
+        }
+   }
+   void Runterfallen ()
+   {
+       switch ( fall )
+       {
+           case 'O':
+                o_item = new ITEM();
+                break;
+           case 'L':
+                l_item = new ITEM();
+                break;
+           case 'S':
+                s_item = new ITEM();
+                break;
+           case 'I':
+                i_item = new ITEM();
+                break;
+           case 'Z':
+                z_item = new ITEM();
+                break;
+           case 'T':
+                t_item = new ITEM();
+                break;
+           case 'J':
+                j_item = new ITEM();
+                break;
+       }
+   }
 }
