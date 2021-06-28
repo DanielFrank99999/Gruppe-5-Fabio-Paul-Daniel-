c6dca825d823d33fe4781f9a8c98e6c2fade54f2
Daniel Frank <danielfrank991@gmail.com>
2021-06-28 21:50:56 +0200

    j

--- J_ITEM.java
+++ J_ITEM.java
@@ -6,11 +6,5 @@ import javax.imageio.*;
 import java.io.*;
 class J_ITEM extends ITEM 
 {
-    protected void paintComponent(Graphics g){
-       
-        repaint();
-    }
-    {
-        g.fillRect(50,50,50,50);
-    }
+    
 }
