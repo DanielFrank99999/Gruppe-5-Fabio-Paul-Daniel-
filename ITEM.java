4887013422e75f2110de3f0a810d070970a75795
Paul1434 <PackPaul@outlook.de>
2021-06-29 08:09:07 +0200

    ITEM

--- ITEM.java
+++ ITEM.java
@@ -1,14 +1,9 @@
 
 abstract class ITEM extends Draw
 {
-   int positionX;
-   int positionY;
-   char fall;
+
    ITEM()
    {
-     positionX = 300;
-     positionY = 0;
-     Runterfallen ();
-     fall = 'O';
+
    } 
 }
