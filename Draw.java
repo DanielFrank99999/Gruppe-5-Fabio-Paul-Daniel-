b3bc78724bf8e6bb33c0813aa77d9c3b10a1add0
Paul1434 <PackPaul@outlook.de>
2021-07-06 09:41:01 +0200

    t

--- Draw.java
+++ Draw.java
@@ -16,10 +16,7 @@ public class Draw extends JLabel
         // g.setColor(Color.black);
         // g.fillRect(0,0,600,800);
         g.setColor(Color.gray);
-        g.fillRect(0,0,600,50);
-        g.fillRect(0,0,10,800);
-        g.fillRect(575,0,600,800);
-        g.fillRect(0,550,600,800);
+        g.fillRect(0,0,600,800);
          g.setColor(Color.white);
         g.drawString(String.valueOf(Clock.temp),560,20);       
         g.setColor(Color.BLACK);
